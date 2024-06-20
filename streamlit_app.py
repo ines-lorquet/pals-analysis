1576e90a38d1e47b6055c8972c036a133f95203e
bruno-coulet <bruno.coulet@laplateforme.io>
2024-06-20 14:46:41 +0200

    Réglage probleme env virtuel

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@
 # Import libraries
 import streamlit as st
 import pandas as pd
-import altair as alt
-import plotly.express as px
+# import altair as alt
+# import plotly.express as px
 
 #######################
 # Page configuration
