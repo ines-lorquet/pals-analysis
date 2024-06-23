22ac08c53b3ae953491a42405d43db9c0fdd0641
bruno-coulet <bruno.coulet@laplateforme.io>
2024-06-23 17:24:27 +0200

    ajout des infos sur .env

--- db.py
+++ db.py
@@ -2,21 +2,26 @@ import mysql.connector
 from dotenv import load_dotenv
 import os
 
+# Loads variables from .env
 load_dotenv()
-# from mysql.connector import Error
-# import pandas as pd
 
-# Configuration de la connexion à la base de données MySQL
+# Configure connection to DB
 db_config = {
-    'host'= DB_HOST,
-    'database'= DB_NAME,
-    'user'= DB_USER,
-    'password'= DB_PASSWORD
+    'host': os.getenv('DB_HOST'),
+    'database': os.getenv('DB_NAME'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD')
 }
 
-# db_config = {
-#     'host': DB_HOST,
-#     'database': DB_NAME,
-#     'user': DB_USER,
-#     'password': DB_PASSWORD
-# }
+# Example 
+try:
+    connection = mysql.connector.connect(**db_config)
+    if connection.is_connected():
+        print("Connexion réussie à la base de données")
+        # REQUESTS
+except mysql.connector.Error as err:
+    print(f"Erreur: {err}")
+finally:
+    if connection.is_connected():
+        connection.close()
+        print("La connexion à la base de données a été fermée")
