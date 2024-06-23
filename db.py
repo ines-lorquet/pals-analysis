29fbd31cd405dc0fe3be4db3a5c588efdcc7e281
bruno-coulet <bruno.coulet@laplateforme.io>
2024-06-23 16:30:10 +0200

    test de fichier .env pour les identifiants de connexion à la BDD

--- db.py
+++ db.py
@@ -1,13 +1,22 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 # from mysql.connector import Error
 # import pandas as pd
 
-
-
 # Configuration de la connexion à la base de données MySQL
 db_config = {
-    'host': 'localhost',
-    'database': 'palworld_database',
-    'user': 'root',
-    'password': 'root'
+    'host'= DB_HOST,
+    'database'= DB_NAME,
+    'user'= DB_USER,
+    'password'= DB_PASSWORD
 }
+
+# db_config = {
+#     'host': DB_HOST,
+#     'database': DB_NAME,
+#     'user': DB_USER,
+#     'password': DB_PASSWORD
+# }
