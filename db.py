09136b501e823e2144d466e45c7e4d68e1d3899d
bruno-coulet <bruno.coulet@laplateforme.io>
2024-06-23 16:13:13 +0200

    embrion de création et connexion à une BDD

---
+++ db.py
@@ -0,0 +1,13 @@
+import mysql.connector
+# from mysql.connector import Error
+# import pandas as pd
+
+
+
+# Configuration de la connexion à la base de données MySQL
+db_config = {
+    'host': 'localhost',
+    'database': 'palworld_database',
+    'user': 'root',
+    'password': 'root'
+}
